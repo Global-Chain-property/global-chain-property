5876fa1dd181dc280934b94d3034cc1ff474c074
miss-naseer <167442258+miss-naseer@users.noreply.github.com>
2025-11-10 20:39:20 +0100

    moodified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,29 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.7s ease-out forwards',
+      },
       colors: {
-        navy: "#222222",
-        gold: "#DEC05F",
-        offwhite: "#F5F5DC",
+        navy: '#222222',
+        gold: '#DEC05F',
+        offwhite: '#F5F5DC',
       },
     },
   },
+  plugins: [forms],
 };
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
- theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-            keyframes: {
-                'fade-in-up': {
-                    '0%': { opacity: '0', transform: 'translateY(20px)' },
-                    '100%': { opacity: '1', transform: 'translateY(0)' },
-                },
-            },
-            animation: {
-                'fade-in-up': 'fade-in-up 0.7s ease-out forwards',
-            },
-            colors: {
-                navy: "#222222",
-                gold: "#DEC05F",
-                offwhite: "#F5F5DC",
-            },
-        },
-    },
- 
- 
-
-    plugins: [forms],
-}
